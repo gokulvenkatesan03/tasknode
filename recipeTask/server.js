f7d061b0b58e7b3c2104aa48a59b164e8c43ceb2
gokulvenkatesan03 <gokulvenkatasan69@gmail.com>
2024-12-06 19:33:53 +0530

    Update server.js

--- recipeTask/server.js
+++ recipeTask/server.js
@@ -1,19 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const connectDB = require('./Database/dbConfig'); // Adjust the path if needed
-const recipeRouter = require('./Routers/recipeRouter');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import recipeRoute from './Routers/recipeRouter.js';
+import connectDB from './Database/dbConfig.js';
 
+//dotenv config
+dotenv.config();
+
+//express declaration
 const app = express();
-app.use(bodyParser.json());
 
-// Connect to the database
+//req.body
+app.use(express.json());
+
+//cors 
+app.use(cors());
+
+//connectDB
 connectDB();
 
-// Routes
-app.use('/api/recipes', recipeRouter);
+//default route
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Welcome to Recipes API"});
+})
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+//default
+app.use("/api/recipes",recipeRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log("Server was Started Successfully");
+})
